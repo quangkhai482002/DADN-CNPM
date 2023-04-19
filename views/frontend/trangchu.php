8e12d7e1acd3c722cdc1a34bdb3982992c6642db
DatYuUxie <118263835+DatYuUxie@users.noreply.github.com>
2023-04-19 23:09:57 +0700

    trang chu

--- views/frontend/trangchu.php
+++ views/frontend/trangchu.php
@@ -1,38 +1,52 @@
-<div class="col-sm-6 col-md-8">
-                    <h1 style="font-size: 20px;">Video Hướng dẫn</h1>
-                    <div class="container">
-                        <div class="col-xs-12 col-sm-6">
-                            <div class="box rte">
-
-                                <h6 class="boxHeadlineSub">Trồng cây</h6>
-                                <iframe width="50%" height="150" src="https://www.youtube.com/embed/seLEvrZ6_6w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                        </div>
-
-                        <div class="col-xs-12 col-sm-6">
-                            <div class="box rte">
-
-                                <h6 class="boxHeadlineSub">Trồng cây</h6>
-                                <iframe width="50%" height="150" src="https://www.youtube.com/embed/seLEvrZ6_6w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                        </div>
-
-
-
-                        <div class="col-xs-12 col-sm-6">
-                            <div class="box rte">
-
-                                <h6 class="boxHeadlineSub">Trồng cây</h6>
-                                <iframe width="50%" height="150" src="https://www.youtube.com/embed/seLEvrZ6_6w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                        </div>
-
-                        <div class="col-xs-12 col-sm-6">
-                            <div class="box rte">
-
-                                <h6 class="boxHeadlineSub">Trồng cây</h6>
-                                <iframe width="50%" height="150" src="https://www.youtube.com/embed/seLEvrZ6_6w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                    </div>
+<div class="displayData-container">
+          <div class="grid">
+            <div class="row">
+              <div class="displayData-1 c-12">
+                <h3 class="dataTitle">THÔNG SỐ MÔI TRƯỜNG</h3>
+                <div class="phankhu-1">
+                  <select class="dropdown-1" placeholder="Please choose">
+                    <option>Phân khu A</option>
+                    <option>Phân khu B</option>
+                    <option>Phân khu C</option>
+                    <option>Phân khu D</option>
+                  </select>
                 </div>
+              </div>
+              <div class="displayData c-12">
+                <div class="data-content c-3">
+                  <div class="AS"><img class="weather-icon" alt="" src="assets/svg/Sun 2.svg" /></div>
+                  <div class="dev-text">
+                    <b class="data-name">ÁNH SÁNG</b>
+                    <b class="data-val">100*C</b>
+                  </div>
+                </div>
+
+                <div class="data-content c-3">
+                    <div class="ND"><img class="weather-icon" alt="" src="assets/svg/Temperature.svg" /></div>
+                    <div class="dev-text">
+                      <b class="data-name">NHIỆT ĐỘ</b>
+                      <b class="data-val">100*C</b>
+                    </div>
+                  </div>
+
+                  <div class="data-content c-3">
+                    <div class="DA"><img class="weather-icon" alt="" src="assets/svg/Waterdrops.svg" /></div>
+                    <div class="dev-text">
+                      <b class="data-name">ĐỘ ẨM</b>
+                      <b class="data-val">100%</b>
+                    </div>
+                  </div>
+                  
+                  <div class="data-content c-3">
+                    <div class="DAD"><img class="weather-icon" alt="" src="assets/svg/Waterdrops-1.svg" /></div>
+                    <div class="dev-text">
+                      <b class="data-name">ĐỘ ÂM ĐẤT</b>
+                      <b class="data-val">100%</b>
+                    </div>
+                  </div>
+              </div>
+
+              
+            </div>
+          </div>
+        </div>
